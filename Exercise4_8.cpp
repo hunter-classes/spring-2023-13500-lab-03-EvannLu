217c54ab82a07c8b0e7ba4ba8091393ad836e749
Evan Lu <evan.l60@myhunter.cuny.edu>
2023-02-12 17:13:33 -0500

    updated

---
+++ Exercise4_8.cpp
@@ -0,0 +1,23 @@
+/*
+Author: Evan Lu
+Course: CSCI-135
+Instructor: Professor Maryash
+Assignment:  Homework Exercise 4.8 - 1 
+*/
+
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string name;
+
+    std::cout << "Enter an word: ";
+    std::cin >> name; 
+
+    for(int i=0; i<name[i]; i++){
+        std::cout << name[i] << std::endl;
+    }
+    
+
+    return 0; 
+}
